ffcf7cc53928d55f6e8087d4585fb79fac59bb60
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2023-03-07 00:06:51 -0600

    React App Created
    UI React App created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <p>Kind Heart Charity</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
